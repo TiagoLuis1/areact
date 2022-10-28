913a0a90c0d2aa97f84eadf466d7aa5d7c8851fb
Tiago Luis <tiagoluis352@gmail.com>
2022-10-28 09:28:25 -0300

    first

---
+++ src/components/contador.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Contador() {
+
+const [count,setCount] = useState(10);
+
+return(
+<div>
+    <p>Voce clicou (count) vezes</p>
+    <button onClick = {() => setCount(count + 1)}>
+    Clique aqui
+    </button>
+</div>
+);
+}
+
+export default Contador
+
+
+
+
+
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoiZBcv9Iv95CWxgQm8yHlnr2HBA4FePg",
+  authDomain: "sitetiago-f18bb.firebaseapp.com",
+  projectId: "sitetiago-f18bb",
+  storageBucket: "sitetiago-f18bb.appspot.com",
+  messagingSenderId: "590174589431",
+  appId: "1:590174589431:web:bf05b54125aa5d4095def2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
