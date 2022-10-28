cefe052098388ae0759c23a73c24e8c3858e7074
Tiago Luis <tiagoluis352@gmail.com>
2022-10-28 11:35:24 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import Contador from "./components/contador";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
 
 function App() {
   return(
+    
     <div>
-     <Button>Entrar</Button>
+ 
+    <Nav justify variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Início</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Página 2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      
+    </Nav>
+  
     </div>
   )
 }
 
 export default App;
+
